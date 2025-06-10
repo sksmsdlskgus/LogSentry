74cd6706e41459bcfc677e857eca995f186da0bd
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-06-10 16:55:54 +0900

    📦 build: gradle 빌드 초기 설정 추가

---
+++ src/main/java/com/nana/logsentry/LogSentryApplication.java
@@ -0,0 +1,13 @@
+package com.nana.logsentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogSentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogSentryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nana/logsentry/LogSentryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nana.logsentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogSentryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
