1933f435de4665278956c389ca1231dbe6681253
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-06-28 16:30:38 +0900

    ✨ feat: 공통 파싱 List 반환 메소드 분리 및 TOP5 요청 IP 조회 (30일 기준으로 5개) Service 기능 추가

--- src/main/java/com/nana/logsentry/log/service/LogAnalyzerService.java
+++ src/main/java/com/nana/logsentry/log/service/LogAnalyzerService.java
@@ -1,5 +1,6 @@
 package com.nana.logsentry.log.service;
 
+import com.nana.logsentry.log.dto.TopIpStatDto;
 import com.nana.logsentry.model.LogEntry;
 import com.nana.logsentry.util.parser.LogParserUtils;
 import org.springframework.stereotype.Service;
@@ -8,8 +9,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Objects;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,19 +20,48 @@ public class LogAnalyzerService {
 
     // 최신 로그 조회 API (오늘 기준으로 최근 100개 가져오기)
     public List<LogEntry> lastLogFile(String date) {
+        return parseLogEntries(date).stream()
+                .sorted(Comparator.comparing(LogEntry::getTimestamp).reversed())
+                .limit(100)
+                .toList();
+    }
+
+    // TOP5 요청 IP 조회 (30일 기준으로 5개 가져오기)
+    public List<TopIpStatDto> getTopLogIp(LocalDate baseDate) {
+        List<LogEntry> allEntries = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            LocalDate date = baseDate.minusDays(i);
+            allEntries.addAll(parseLogEntries(date.toString()));
+        }
+
+        return allEntries.stream()
+                .collect(Collectors.groupingBy(LogEntry::getClientIp, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(5)
+                .map(e -> new TopIpStatDto(e.getKey(), Math.toIntExact(e.getValue())))
+                .collect(Collectors.toList());
+    }
+
+
+    // 공통 파싱 후 List 반환
+    private List<LogEntry> parseLogEntries(String date) {
         String logFile = String.format("logs/text/client-requests.%s.log", date);
         Path logPath = Paths.get(logFile);
+
+        if (!Files.exists(logPath)) {
+            return new ArrayList<>(); // 해당 날짜의 로그 파일이 없으면 빈 리스트 반환
+        }
 
         try {
             return Files.lines(logPath)
                     .map(parser::parse)
                     .filter(Objects::nonNull)
-                    .limit(100) // 오늘 기준으로 100개 제한
-                    .collect(Collectors.toList());
+                    .toList();
         } catch (IOException e) {
-            throw new RuntimeException("로그 파일 읽기 실패: " + logFile, e);
+            return Collections.emptyList(); // 하나의 파일 읽기에 실패해도 전체 중단은 방지
         }
     }
-
 
 }
