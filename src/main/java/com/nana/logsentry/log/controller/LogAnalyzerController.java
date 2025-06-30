44ffcd6f443c1c76c06fd426f24270b1a08b06c0
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-06-30 14:36:16 +0900

    ✨ feat: 최신 로그 100개 조회 Controller 작성

--- src/main/java/com/nana/logsentry/log/controller/LogAnalyzerController.java
+++ src/main/java/com/nana/logsentry/log/controller/LogAnalyzerController.java
@@ -1,16 +1,36 @@
 package com.nana.logsentry.log.controller;
 
+import com.nana.logsentry.log.service.LogAnalyzerService;
+import com.nana.logsentry.model.LogEntry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Tag(name = "로그 통계", description = "로그 파일 분석 및 통계 조회 API")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/logs")
 public class LogAnalyzerController {
 
-    // 최신 로그 조회 API -> GET /api/logs/latest?limit=10
-    // TOP5 요청 IP 조회 API -> GET /api/logs/top/ip
-    // TOP5 요청 URL 조회 API -> GET /api/logs/top/uri
-    // 로그 파일 날짜 목록 조회 -> GET /api/logs/dates
+    private final LogAnalyzerService logAnalyzerService;
+
+    @Operation(summary = "최신 로그 조회",
+            description = "지정한 날짜의 로그 파일에서 최대 100개의 최신 요청 로그를 조회합니다.",
+            responses = { @ApiResponse(responseCode = "200", description = "정상적으로 로그를 조회했습니다."),
+                    @ApiResponse(responseCode = "500", description = "서버 오류로 인해 로그를 조회하지 못했습니다.")})
+    @GetMapping("/latest")
+    public List<LogEntry> getLatestLogs(@RequestParam(name = "date") String date) {
+        return logAnalyzerService.getLatestLogs(date);
+    }
+
+
 
 
 }
