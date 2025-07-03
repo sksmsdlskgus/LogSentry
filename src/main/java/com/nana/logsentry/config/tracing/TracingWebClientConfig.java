fc0a69ef21f44a77845ce6cb6667c9588898cc07
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-07-03 18:42:46 +0900

    🔨 refactor: WebClient 필터를 Micrometer Tracer 기반 traceId/spanId 로깅 중심으로 간소화

--- src/main/java/com/nana/logsentry/config/tracing/TracingWebClientConfig.java
+++ src/main/java/com/nana/logsentry/config/tracing/TracingWebClientConfig.java
@@ -2,41 +2,26 @@ package com.nana.logsentry.config.tracing;
 
 
 import io.micrometer.tracing.Tracer;
-import io.micrometer.tracing.propagation.Propagator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
 
 @Configuration
+@Slf4j
 public class TracingWebClientConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(TracingWebClientConfig.class);
-
     @Bean
-    public WebClient webClient(WebClient.Builder builder, Tracer tracer, Propagator propagator) {
+    public WebClient webClient(WebClient.Builder builder, Tracer tracer) {
         return builder
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()))
                 .filter((request, next) -> {
-                    // 수동으로 trace 정보를 헤더에 삽입
-                    return next.exchange(
-                            ClientRequest.from(request)
-                                    .headers(httpHeaders -> {
-                                        var context = tracer.currentTraceContext().context();
-                                        if (context != null) {
-                                            propagator.inject(context, httpHeaders, HttpHeaders::set);
-                                            log.info("[Tracing] WebClient 요청에 traceId={} spanId={}",
-                                                    context.traceId(), context.spanId());
-                                        }
-                                    })
-                                    .build()
-                    );
+                    var context = tracer.currentTraceContext().context();
+                    if (context != null) {
+                        log.info("[Tracing] traceId={}, spanId={}", context.traceId(), context.spanId());
+                    }
+                    return next.exchange(request);
                 })
                 .build();
     }
+
 }
