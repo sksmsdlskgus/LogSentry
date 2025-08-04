cfc339c90060b8ba4b4fd5737d89ee43d8385a02
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-08-04 14:18:16 +0900

    ✨ feat: TestController /test 로그에 Kafka 전송 추가

--- src/main/java/com/nana/logsentry/TestController.java
+++ src/main/java/com/nana/logsentry/TestController.java
@@ -1,8 +1,10 @@
 package com.nana.logsentry;
 
+import com.nana.logsentry.kafka.BizLogger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +14,9 @@ public class TestController {
     // http://localhost:8081/test
     @GetMapping("/test")
     public ResponseEntity<String> test() {
-        log.info("✅ 로그 트레이싱 테스트 요청 수신됨");
-        return ResponseEntity.ok("트레이싱 확인용 응답");
+        log.info("✅ 로컬 전용 로그");
+        BizLogger.info("✅ Kafka 전송용 로그: /test 호출됨");
+        return ResponseEntity.ok("트레이싱 확인용 응답 ok");
     }
 
 }
