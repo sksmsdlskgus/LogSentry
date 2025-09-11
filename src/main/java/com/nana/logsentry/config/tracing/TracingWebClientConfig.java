67e50dfaaa4d6f99ea72695671eb2d8fba4370d7
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-09-11 14:47:24 +0900

    🔧 config: TracingWebClientConfig에 BProps 적용하여 WebClient 기본 URL 설정

--- src/main/java/com/nana/logsentry/config/tracing/TracingWebClientConfig.java
+++ src/main/java/com/nana/logsentry/config/tracing/TracingWebClientConfig.java
@@ -2,18 +2,24 @@ package com.nana.logsentry.config.tracing;
 
 
 import io.micrometer.tracing.Tracer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@RequiredArgsConstructor
 @Slf4j
 public class TracingWebClientConfig {
 
+    private final BProps props;
+    private final Tracer tracer;
+
     @Bean
-    public WebClient webClient(WebClient.Builder builder, Tracer tracer) {
+    WebClient webClient(WebClient.Builder builder) {
         return builder
+                .baseUrl(props.baseUrl())
                 .filter((request, next) -> {
                     var context = tracer.currentTraceContext().context();
                     if (context != null) {
