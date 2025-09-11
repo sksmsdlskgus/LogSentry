ad0e27a694c669bae9dca8b0d06bc65e240e883a
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-09-11 14:46:36 +0900

    🔧 config: LogSentryApplication에 BProps 설정 바인딩 활성화

--- src/main/java/com/nana/logsentry/LogSentryApplication.java
+++ src/main/java/com/nana/logsentry/LogSentryApplication.java
@@ -1,10 +1,13 @@
 package com.nana.logsentry;
 
+import com.nana.logsentry.config.tracing.BProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BProps.class)
 @EnableScheduling //스케줄링 활성화
 public class LogSentryApplication {
 
